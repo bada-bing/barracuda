3cecfc1085f573cf172cfb91298f72c2df87d8bd
Miki <bada.bing.bing@live.com>
2024-02-15 19:23:00 +0100

    ✨ [Lexing] Add token package
    - add TokenType and Token struct

---
+++ token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL" // unknown token
+	EOF     = "EOF"
+
+	// identifiers and literals
+	IDENTIF = "IDENTIF" // a, aa, aab, ...
+	INT     = "INT"     // 1, 2, 3, ...
+
+	// operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// delimitters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+}
+
+func LookupIdentif(identifier string) TokenType {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+	return IDENTIF
+}
