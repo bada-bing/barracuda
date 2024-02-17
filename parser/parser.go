20c6caa25546dc8ebb3fbdaea4d15f2f6ae70bd9
Miki <bada.bing.bing@live.com>
2024-02-17 22:08:24 +0100

    ✨ [Parsing] Add error handling to the parser

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"barracuda/ast"
 	"barracuda/lexer"
 	"barracuda/token"
+	"fmt"
 )
 
 type Parser struct {
@@ -11,10 +12,12 @@ type Parser struct {
 
 	currentToken token.Token
 	peekToken    token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// read two tokens so that both current and peek token are set
 	p.NextToken()
@@ -22,6 +25,15 @@ func New(l *lexer.Lexer) *Parser {
 
 	return p
 
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) NextToken() {
@@ -85,6 +97,7 @@ func (p *Parser) expectPeekTokenType(t token.TokenType) bool {
 		p.NextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
